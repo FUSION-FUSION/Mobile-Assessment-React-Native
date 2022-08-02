c3c545c67a7305700741421eb988d7bb678162f3
RufixDuke <rufixduke01@gmail.com>
2022-08-02 09:12:28 +0100

    restructuring the asset files

---
+++ Navigator.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import SplashScreen from "./screens/SplashScreen";
+import VerificationScreen from "./screens/VerificationScreen";
+import LocationScreen from "./screens/LocationScreen";
+import UserSelectionScreen from "./screens/UserSelectionScreen";
+import CongratVerifiedScreen from "./screens/CongratVerifiedScreen";
+
+const Stack = createNativeStackNavigator();
+
+const screenOptions = {
+    headerShown: false,
+};
+
+export const MyStack = () => (
+    <NavigationContainer>
+        <Stack.Navigator
+            initialRouteName="SplashScreen"
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+            <Stack.Screen name="SplashScreen" component={SplashScreen} />
+            <Stack.Screen
+                name="VerificationScreen"
+                component={VerificationScreen}
+            />
+            <Stack.Screen name="LocationScreen" component={LocationScreen} />
+            <Stack.Screen
+                name="UserSelectionScreen"
+                component={UserSelectionScreen}
+            />
+            <Stack.Screen
+                name="CongratVerifiedScreen"
+                component={CongratVerifiedScreen}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,10 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
 
 const SplashScreen = () => {
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         navigation.replace("SwiperScreen");
+    //     }, 5000);
+    // });
     return (
-        <View>
-            <Text>SplashScreen</Text>
+        <View
+            style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "black",
+            }}
+        >
+            <Image
+                source={require("../assets/Shipify-Assets/splash_logo.png")}
+            />
         </View>
     );
 };
