ae18a6a7d453bb14e22294173dda01f1e3c3e895
RufixDuke <rufixduke01@gmail.com>
2022-08-02 19:57:35 +0100

    Got a necessary asset to use on the home screen

--- components/Card.js
+++ components/Card.js
@@ -15,6 +15,7 @@ const Card = () => {
                     backgroundColor: "#ffffff",
                     width: "45%",
                     height: "40%",
+                    position: "relative",
                 }}
             >
                 <Text>Same State</Text>
@@ -45,9 +46,32 @@ const Card = () => {
                         }}
                     />
                 </View>
+
+                <View
+                    style={{
+                        position: "absolute",
+                        bottom: 0,
+                        right: 0,
+                        backgroundColor: "#ffffff",
+                    }}
+                >
+                    <Image
+                        source={require("../assets/Shipify-Assets/ic-right.png")}
+                        style={{
+                            width: 20,
+                            height: 20,
+                        }}
+                    />
+                </View>
             </View>
 
-            <View style={{ backgroundColor: "#ffffff", width: "45%" }}>
+            <View
+                style={{
+                    backgroundColor: "#ffffff",
+                    width: "45%",
+                    height: "40%",
+                }}
+            >
                 <Text>Same State</Text>
                 <Text
                     style={{
@@ -56,9 +80,26 @@ const Card = () => {
                         borderRightWidth: 0,
                         borderLeftWidth: 0,
                         borderColor: "#46a5ba",
-                        width: "5%",
+                        width: "10%",
+                        height: "7%",
+                        marginTop: 2,
                     }}
                 ></Text>
+                <Text>Deliveries within the same state</Text>
+                <View style={{ width: "100%" }}>
+                    <Image
+                        source={require("../assets/Shipify-Assets/ic-road-interstate.png")}
+                        style={{ width: "100%", height: "60%" }}
+                    />
+                    <Image
+                        source={require("../assets/Shipify-Assets/Delivery-Van.png")}
+                        style={{
+                            width: "80%",
+                            height: "45%",
+                            top: "-50%",
+                        }}
+                    />
+                </View>
             </View>
         </View>
     );
