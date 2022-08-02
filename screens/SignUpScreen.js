566ffaf907a9c19e8757b9a960bc4209c5e8edd5
RufixDuke <rufixduke01@gmail.com>
2022-08-02 12:23:37 +0100

    Finished with signup screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,12 +1,185 @@
-import { View, Text } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    TextInput,
+    Pressable,
+} from "react-native";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [number, setNumber] = useState("+234");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
     return (
-        <View>
-            <Text>SignUpScreen</Text>
+        <View style={styles.wrapper}>
+            <LinearGradient
+                colors={["#dbe4e9", "#f0f0f3"]}
+                // style={{ flex: 1 }}
+            >
+                <ImageBackground
+                    source={require("../assets/Shipify-Assets/bg-app-cloud.png")}
+                    style={styles.background}
+                >
+                    <View style={styles.container}>
+                        <Text style={styles.text}>Welcome!</Text>
+                        <Text style={styles.smallerText}>
+                            Create an account to get started with Cargo Express
+                        </Text>
+                    </View>
+
+                    <View style={styles.inputWrapper}>
+                        <Text>Full Name</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(value) => setName(value)}
+                            value={name}
+                        />
+                    </View>
+
+                    <View style={styles.inputWrapper}>
+                        <Text>Your E-mail</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(value) => setEmail(value)}
+                            value={email}
+                        />
+                    </View>
+
+                    <View style={styles.inputWrapper}>
+                        <Text>Phone Number</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(value) => setNumber(value)}
+                            value={number}
+                            keyboardType="numeric"
+                        />
+                    </View>
+
+                    <View style={styles.inputWrapper}>
+                        <Text>Password</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(value) => setPassword(value)}
+                            value={password}
+                        />
+                    </View>
+
+                    <View style={styles.inputWrapper}>
+                        <Text>Confirm Password</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(value) => setConfirm(value)}
+                            value={confirm}
+                        />
+                    </View>
+
+                    <Text style={{ textAlign: "center", paddingTop: 20 }}>
+                        Already have an account?
+                        <Text style={{ color: "#46a5ba", fontWeight: "900" }}>
+                            {" "}
+                            Log in
+                        </Text>
+                    </Text>
+
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            marginHorizontal: 10,
+                            justifyContent: "space-between",
+                            paddingTop: 30,
+                        }}
+                    >
+                        <Pressable
+                            style={styles.btn1}
+                            onPress={() => navigation.goBack()}
+                        >
+                            <Text style={styles.btnText1}>Back</Text>
+                        </Pressable>
+
+                        <Pressable
+                            style={styles.btn2}
+                            onPress={() =>
+                                navigation.navigate("VerificationScreen")
+                            }
+                        >
+                            <Text style={styles.btnText2}>Next</Text>
+                        </Pressable>
+                    </View>
+                </ImageBackground>
+            </LinearGradient>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+    },
+    background: {
+        width: "100%",
+        height: 300,
+        // backgroundColor: "#dbe4e9",
+    },
+    container: {
+        paddingTop: 100,
+        width: "60%",
+        marginLeft: 20,
+    },
+    text: {
+        fontSize: 25,
+        fontWeight: "900",
+        color: "black",
+    },
+    smallerText: {
+        fontSize: 15,
+    },
+    input: {
+        width: "100%",
+        borderColor: "#555555",
+        borderRadius: 15,
+        backgroundColor: "#ffffff",
+        textAlign: "left",
+        fontSize: 14,
+        marginTop: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+    },
+    inputWrapper: {
+        marginHorizontal: 20,
+        paddingTop: 20,
+    },
+    btn2: {
+        backgroundColor: "#46a5ba",
+        paddingVertical: 15,
+        borderRadius: 15,
+        marginHorizontal: 20,
+        width: "30%",
+    },
+    btn1: {
+        backgroundColor: "#fff",
+        paddingVertical: 15,
+        borderRadius: 15,
+        marginHorizontal: 20,
+        width: "30%",
+    },
+    btnText2: {
+        textAlign: "center",
+        color: "#FFFFFF",
+        fontSize: 20,
+        fontWeight: "900",
+    },
+    btnText1: {
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "900",
+        color: "#737373",
+    },
+});
 
 export default SignUpScreen;
