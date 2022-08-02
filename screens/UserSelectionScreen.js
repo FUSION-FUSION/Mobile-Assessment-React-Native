e664ef29df34389cd9a4b1da7add12f8833fb6e6
RufixDuke <rufixduke01@gmail.com>
2022-08-02 11:03:57 +0100

    Uninstall and Re-installed a new linear gradient dependency

--- screens/UserSelectionScreen.js
+++ screens/UserSelectionScreen.js
@@ -5,85 +5,87 @@ import { LinearGradient } from "expo-linear-gradient";
 const UserSelectionScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1 }}>
-            <ImageBackground
-                source={require("../assets/Shipify-Assets/bg-app-cloud.png")}
-                style={{
-                    width: "100%",
-                    height: 300,
-                    backgroundColor: "#dbe4e9",
-                }}
-            >
-                <LinearGradient colors={["#dbe4e9", "#f0f0f3"]} />
-                <View style={{ paddingTop: 80, width: "60%", marginLeft: 20 }}>
-                    <Text
-                        style={{
-                            fontSize: 25,
-                            fontWeight: "900",
-                            color: "black",
-                        }}
-                    >
-                        What kind of user are you?
-                    </Text>
-                    <Text
-                        style={{
-                            fontSize: 16,
-                        }}
-                    >
-                        We will adapt the app to suit your needs.
-                    </Text>
-                </View>
-
-                <View
+            <LinearGradient colors={["#dbe4e9", "#f0f0f3"]} style={{ flex: 1 }}>
+                <ImageBackground
+                    source={require("../assets/Shipify-Assets/bg-app-cloud.png")}
                     style={{
-                        position: "absolute",
-                        bottom: "-60%",
                         width: "100%",
+                        height: 300,
+                        backgroundColor: "#dbe4e9",
                     }}
                 >
-                    <Pressable
-                        style={{
-                            backgroundColor: "#46a5ba",
-                            paddingVertical: 30,
-                            borderRadius: 20,
-                            marginHorizontal: 20,
-                            marginBottom: 30,
-                        }}
-                        onPress={() => navigation.navigate("SignUpScreen")}
+                    <View
+                        style={{ paddingTop: 80, width: "60%", marginLeft: 20 }}
                     >
                         <Text
                             style={{
-                                textAlign: "center",
-                                color: "#FFFFFF",
-                                fontSize: 40,
+                                fontSize: 25,
                                 fontWeight: "900",
+                                color: "black",
                             }}
                         >
-                            Personal
+                            What kind of user are you?
                         </Text>
-                    </Pressable>
-                    <Pressable
-                        style={{
-                            backgroundColor: "#46a5ba",
-                            paddingVertical: 30,
-                            borderRadius: 20,
-                            marginHorizontal: 20,
-                        }}
-                        onPress={() => navigation.navigate("SignUpScreen")}
-                    >
                         <Text
                             style={{
-                                textAlign: "center",
-                                color: "#FFFFFF",
-                                fontSize: 40,
-                                fontWeight: "900",
+                                fontSize: 16,
                             }}
                         >
-                            E-commerce
+                            We will adapt the app to suit your needs.
                         </Text>
-                    </Pressable>
-                </View>
-                {/* </LinearGradient> */}
-            </ImageBackground>
+                    </View>
+
+                    <View
+                        style={{
+                            position: "absolute",
+                            bottom: "-60%",
+                            width: "100%",
+                        }}
+                    >
+                        <Pressable
+                            style={{
+                                backgroundColor: "#46a5ba",
+                                paddingVertical: 30,
+                                borderRadius: 20,
+                                marginHorizontal: 20,
+                                marginBottom: 30,
+                            }}
+                            onPress={() => navigation.navigate("SignUpScreen")}
+                        >
+                            <Text
+                                style={{
+                                    textAlign: "center",
+                                    color: "#FFFFFF",
+                                    fontSize: 40,
+                                    fontWeight: "900",
+                                }}
+                            >
+                                Personal
+                            </Text>
+                        </Pressable>
+                        <Pressable
+                            style={{
+                                backgroundColor: "#46a5ba",
+                                paddingVertical: 30,
+                                borderRadius: 20,
+                                marginHorizontal: 20,
+                            }}
+                            onPress={() => navigation.navigate("SignUpScreen")}
+                        >
+                            <Text
+                                style={{
+                                    textAlign: "center",
+                                    color: "#FFFFFF",
+                                    fontSize: 40,
+                                    fontWeight: "900",
+                                }}
+                            >
+                                E-commerce
+                            </Text>
+                        </Pressable>
+                    </View>
+                </ImageBackground>
+            </LinearGradient>
         </View>
     );
 };
