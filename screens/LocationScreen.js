5a34b669375549cca0523efe23fa8a84cbb54060
RufixDuke <rufixduke01@gmail.com>
2022-08-03 20:12:01 +0100

    Making use of stylesheet to group my styles together so as not to make the code less readable

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -1,10 +1,15 @@
-import { View, Text, Image, ImageBackground, Pressable } from "react-native";
-import React from "react";
-import { StyleSheet } from "react-native";
+import {
+    View,
+    Text,
+    Image,
+    ImageBackground,
+    Pressable,
+    StyleSheet,
+} from "react-native";
+import React, { useState } from "react";
 import Line from "../components/Line";
 import Details from "../components/Details";
 import UncheckedDetails from "../components/UncheckedDetails";
-import { useState } from "react";
 
 const LocationScreen = ({ navigation }) => {
     const [padding, setPadding] = useState("150%");
@@ -25,23 +30,9 @@ const LocationScreen = ({ navigation }) => {
                 source={require("../assets/Shipify-Assets/map.jpeg")}
                 style={{ flex: 1 }}
             >
-                <View
-                    style={{
-                        flexDirection: "row",
-                        marginLeft: 20,
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        paddingTop: 55,
-                        paddingBottom: padding,
-                    }}
-                >
+                <View style={styles.header}>
                     <Pressable
-                        style={{
-                            backgroundColor: "#ffffff",
-                            paddingHorizontal: 5,
-                            paddingVertical: 5,
-                            borderRadius: 10,
-                        }}
+                        style={styles.backBtn}
                         onPress={() => navigation.goBack()}
                     >
                         <Image
@@ -50,38 +41,15 @@ const LocationScreen = ({ navigation }) => {
                         />
                     </Pressable>
 
-                    <View
-                        style={{
-                            backgroundColor: "#ffffff",
-                            paddingHorizontal: 25,
-                            paddingVertical: 8,
-                            borderRadius: 10,
-                        }}
-                    >
+                    <View style={styles.headerText}>
                         <Text>HN-INT-D9FD00-JBW-ORI</Text>
                     </View>
 
                     <Text style={{ marginLeft: 15 }}></Text>
                 </View>
 
-                <Pressable
-                    style={{
-                        backgroundColor: "#ffffff",
-                        paddingHorizontal: 18,
-                        paddingBottom: 20,
-                        borderTopLeftRadius: 30,
-                        borderTopRightRadius: 30,
-                        height: "90%",
-                    }}
-                    onPress={() => toggle()}
-                >
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                        }}
-                    >
+                <Pressable style={styles.toggleField} onPress={() => toggle()}>
+                    <View style={styles.toggleBtn}>
                         <Text></Text>
 
                         <Image
@@ -94,20 +62,8 @@ const LocationScreen = ({ navigation }) => {
                         />
                     </View>
 
-                    <View
-                        style={{
-                            backgroundColor: "#46a5ba",
-                            paddingVertical: 15,
-                            paddingHorizontal: 10,
-                            borderRadius: 10,
-                        }}
-                    >
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                justifyContent: "space-between",
-                            }}
-                        >
+                    <View style={styles.position}>
+                        <View style={styles.locationFlex}>
                             <Text style={{ color: "#ffffff" }}>Pickup</Text>
                             <Text style={{ color: "#ffffff" }}>
                                 Jibowu Terminal
@@ -118,29 +74,12 @@ const LocationScreen = ({ navigation }) => {
                             <Text style={{ color: "#abc7ce" }}>Completed</Text>
                         </View>
 
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                // justifyContent: "space-between",
-                                alignItems: "center",
-                                marginTop: 5,
-                            }}
-                        >
+                        <View style={styles.imgPos}>
                             <Image
                                 source={require("../assets/Shipify-Assets/round-white.png")}
                                 style={{ width: 15, height: 15 }}
                             />
-                            <Text
-                                style={{
-                                    borderWidth: 3,
-                                    borderColor: "#ffffff",
-                                    borderBottomWidth: 0,
-                                    borderRightWidth: 0,
-                                    width: 70,
-                                    borderLeftWidth: 0,
-                                    top: 8,
-                                }}
-                            ></Text>
+                            <Text style={styles.line}></Text>
                             <Image
                                 source={require("../assets/Shipify-Assets/round-white.png")}
                                 style={{ width: 15, height: 15 }}
@@ -161,13 +100,7 @@ const LocationScreen = ({ navigation }) => {
                             />
                         </View>
 
-                        <View
-                            style={{
-                                marginTop: 20,
-                                flexDirection: "row",
-                                justifyContent: "space-between",
-                            }}
-                        >
+                        <View style={styles.date}>
                             <Text style={{ color: "#9ccbd6" }}>
                                 04 Mar, 2022
                             </Text>
@@ -197,15 +130,67 @@ const LocationScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        marginLeft: 20,
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingTop: 55,
+        paddingBottom: padding,
+    },
+    backBtn: {
+        backgroundColor: "#ffffff",
+        paddingHorizontal: 5,
+        paddingVertical: 5,
+        borderRadius: 10,
+    },
+    headerText: {
+        backgroundColor: "#ffffff",
+        paddingHorizontal: 25,
+        paddingVertical: 8,
+        borderRadius: 10,
+    },
+    toggleField: {
+        backgroundColor: "#ffffff",
+        paddingHorizontal: 18,
+        paddingBottom: 20,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        height: "90%",
+    },
+    toggleBtn: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    position: {
+        backgroundColor: "#46a5ba",
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+    },
+    locationFlex: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    imgPos: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 5,
+    },
     line: {
-        borderWidth: 1,
+        borderWidth: 3,
         borderColor: "#ffffff",
         borderBottomWidth: 0,
         borderRightWidth: 0,
-        width: 5,
+        width: 70,
         borderLeftWidth: 0,
         top: 8,
-        marginRight: 2,
+    },
+    date: {
+        marginTop: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
 });
 
